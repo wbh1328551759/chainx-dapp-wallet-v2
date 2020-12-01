6c4df21e1befc0fc3370dcf574a57f242b241e79
吴wbh1328551759 <1328551759@qq.com>
2020-12-01 18:35:19 +0800

    add setInterval in dex and myaccount

--- packages/page-trade/src/hooks/useFills.ts
+++ packages/page-trade/src/hooks/useFills.ts
@@ -1,5 +1,4 @@
-
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
 import {useApi} from '@polkadot/react-hooks';
 
@@ -7,27 +6,32 @@ export default function useFills(): Fill[] {
   const api = useApi();
   const [state, setState] = useState<Fill[]>([]);
   let fillTimeId: any = '';
-
-  useEffect((): void => {
-    async function fetchFills() {
-      const testOrMain = await api.api.rpc.system.properties();
-      const testOrMainNum = JSON.parse(testOrMain);
-      let res;
-      if (testOrMainNum.ss58Format === 42) {
-        res = await axios.get('http://8.210.38.126:3214/dex/fills/0');
-        console.log('1')
-      } else {
-        res = await axios.get('https://api-v2.chainx.org/dex/fills/0');
-      }
-      setState([
-        ...res.data.items
-      ]);
+  async function fetchFills() {
+    const testOrMain = await api.api.rpc.system.properties();
+    const testOrMainNum = JSON.parse(testOrMain);
+    let res;
+    if (testOrMainNum.ss58Format === 42) {
+      res = await axios.get('http://8.210.38.126:3214/dex/fills/0');
+      console.log('1');
+    } else {
+      res = await axios.get('https://api-v2.chainx.org/dex/fills/0');
     }
+    setState([
+      ...res.data.items
+    ]);
+  }
 
+  useEffect(() => {
+    fetchFills()
+  }, [])
+
+  useEffect(() => {
     fillTimeId = setInterval(() => {
       fetchFills();
     }, 5000);
-    }, [fillTimeId]);
+
+    return () => window.clearInterval(fillTimeId)
+  }, [fillTimeId]);
 
   return state;
 }
--- packages/react-hooks-chainx/src/useAsksBids.ts
+++ packages/react-hooks-chainx/src/useAsksBids.ts
@@ -21,28 +21,32 @@ interface OpenOders {
 export default function useAsksBids(): OpenOders {
   const api = useApi();
   const [state, setState] = useState<OpenOders>({Asks: [], Bids: []});
-  let AskBidTimeId: any = '';
-  useEffect(() => {
-    async function fetchAskBid() {
-      const testOrMain = await api.api.rpc.system.properties();
-      const testOrMainNum = JSON.parse(testOrMain);
-      let res;
-      if (testOrMainNum.ss58Format === 42) {
-        res = await axios.get('http://8.210.38.126:3214/dex/depth/0?cnt=6');
-      } else {
-        res = await axios.get('https://api-v2.chainx.org/dex/depth/0');
-      }
-      setState({
-        Asks: res.data.asks.filter((ask: Ask) => ask.amount !== 0),
-        Bids: res.data.bids.filter((bid: Bid) => bid.amount !== 0)
-      });
+  let askBidTimeId: any = '';
+  async function fetchAskBid() {
+    const testOrMain = await api.api.rpc.system.properties();
+    const testOrMainNum = JSON.parse(testOrMain);
+    let res;
+    if (testOrMainNum.ss58Format === 42) {
+      res = await axios.get('http://8.210.38.126:3214/dex/depth/0?cnt=6');
+    } else {
+      res = await axios.get('https://api-v2.chainx.org/dex/depth/0');
     }
+    setState({
+      Asks: res.data.asks.filter((ask: Ask) => ask.amount !== 0),
+      Bids: res.data.bids.filter((bid: Bid) => bid.amount !== 0)
+    });
+  }
 
-    AskBidTimeId = setInterval(() => {
+  useEffect(() => {
+    fetchAskBid()
+  },[])
+
+  useEffect(() => {
+    askBidTimeId = setInterval(() => {
       fetchAskBid();
     }, 5000);
-    return () => window.clearInterval(AskBidTimeId);
-  }, [AskBidTimeId]);
+    return () => window.clearInterval(askBidTimeId);
+  }, [askBidTimeId]);
 
   return state;
 }
