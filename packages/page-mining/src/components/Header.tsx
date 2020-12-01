41c11c0eb07c56d5f8896a20ad4755f373b69e8e
吴wbh1328551759 <1328551759@qq.com>
2020-12-01 17:20:19 +0800

    remove button and withdrawal

--- packages/page-mining/src/components/Header.tsx
+++ packages/page-mining/src/components/Header.tsx
@@ -38,47 +38,7 @@ export default function ({ accounts, allAssets, interestTotal }: Props): React.R
           width='30' />
         <h6>X-BTC</h6>
       </div>
-      <div className='right'>
-        <span><span>{t('Total interest on all accounts')}：</span><span>{Number(interestTotal) / Math.pow(10, 8)} PCX</span></span>
-        <Button
-          icon='plus'
-          label={t<string>('Withdraw interest')}
-          onClick={toggleWithDraw} />
-        {isWithDraw && (
-          <Modal
-            header={t('Withdrawal application of interest')}
-            size='large'
-          >
-            <Modal.Content>
-              <Modal.Columns>
-                <Modal.Column>
-                  <InputAddress
-                    defaultValue={currentAccount}
 
-                    help={t<string>('Select the account you want to withdrawal')}
-                    label={t('Select the account for withdrawal')}
-                    labelExtra={
-                      <span>  {t('avaliable interest')} : {Number(allAssets.find((item) => item.account === accountId)?.XbtcInterests) / Math.pow(10, 8)} </span>
-                    }
-                    onChange={setAccountId}
-                    type='account'
-                    withLabel
-                  />
-                </Modal.Column>
-                <Modal.Actions onCancel={toggleWithDraw}>
-                  <TxButton
-                    accountId={accountId}
-                    icon='plus'
-                    label={t('Withdraw interest')}
-                    params={[1]}
-                    tx='xMiningAsset.claim'
-                  />
-                </Modal.Actions>
-              </Modal.Columns>
-            </Modal.Content>
-          </Modal>)
-        }
-      </div>
     </header>
   );
 }
