ac415887c0ef4b2ecb971182176b35febb588611
吴wbh1328551759 <1328551759@qq.com>
2020-12-08 10:23:21 +0800

    add button of addAccount in nav

--- packages/apps/src/Menu/NodeInfo.tsx
+++ packages/apps/src/Menu/NodeInfo.tsx
@@ -5,37 +5,49 @@ import {BareProps as Props} from '@polkadot/react-components/types';
 
 import React, {useContext} from 'react';
 import styled from 'styled-components';
-import {useApi} from '@polkadot/react-hooks';
+import {useAccounts, useApi, useIpfs, useToggle} from '@polkadot/react-hooks';
 import {useLocalStorage} from '@polkadot/react-hooks-chainx';
 import {NodeName, NodeVersion} from '@polkadot/react-query';
 import AccountStatus from '@polkadot/react-components-chainx/AccountStatus';
-import {StatusContext} from '@polkadot/react-components';
+import {Button, StatusContext} from '@polkadot/react-components';
+import CreateModal from '@polkadot/app-accounts-chainx/modals/Create';
+import {useTranslation} from '@polkadot/app-accounts-chainx/translate';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const pkgJson = require('../../package.json') as { version: string };
 
-const uiInfo = `apps v${pkgJson.version}`;
 
 function NodeInfo({className = ''}: Props): React.ReactElement<Props> {
-  const {api, isApiReady} = useApi();
+  const {t} = useTranslation();
+  const {hasAccounts} = useAccounts();
+  const {isApiReady} = useApi();
   let [storedValue, setValue] = useLocalStorage<string>('currentAccount');
-  const { queueAction } = useContext(StatusContext);
+  const {queueAction} = useContext(StatusContext);
+  const [isCreateOpen, toggleCreate] = useToggle();
+  const {isIpfs} = useIpfs();
 
   return (
     <div className={`${className} media--1400 highlight--color-contrast`}>
-      {isApiReady && (
-        <AccountStatus
-          storedValue={storedValue}
+      {isCreateOpen && (
+        <CreateModal
+          onClose={toggleCreate}
           onStatusChange={queueAction}
-          setStoredValue={setValue}
         />
-        // <div>
-        //   <NodeName/>&nbsp;
-        //   <NodeVersion label='v'/>
-        // </div>
       )}
-      {/*<div>{api.libraryInfo.replace('@polkadot/', '')}</div>*/}
-      {/*<div>{uiInfo}</div>*/}
+
+      {isApiReady && (hasAccounts ?
+          <AccountStatus
+            storedValue={storedValue}
+            onStatusChange={queueAction}
+            setStoredValue={setValue}
+          /> :
+          <Button
+            icon='plus'
+            isDisabled={isIpfs}
+            label={t<string>('Add account')}
+            onClick={toggleCreate}
+          />
+      )
+      }
 
     </div>
   );
