bf79328620f43b7ac9a4a8bed8def24a2f7b1dcf
吴wbh1328551759 <1328551759@qq.com>
2020-12-15 18:56:25 +0800

    fix askbid

--- packages/react-hooks-chainx/src/useAsksBids.ts
+++ packages/react-hooks-chainx/src/useAsksBids.ts
@@ -21,20 +21,12 @@ export default function useAsksBids(isLoading: boolean): OpenOrders {
   const [state, setState] = useState<OpenOrders>({Asks: [], Bids: []});
   let askBidTimeId: any = '';
   async function fetchAskBid() {
-    // const testOrMain = await api.api.rpc.system.properties();
-    // const testOrMainNum = JSON.parse(testOrMain);
-    // let res;
-    // if (testOrMainNum.ss58Format === 42) {
-    //   res = await axios.get('https://testnet-api.chainx.org/dex/depth/0?cnt=6');
-    // } else {
-    //   res = await axios.get('https://api-v2.chainx.org/dex/depth/0');
-    // }
     const res = await api.api.rpc.xspot.getDepth(0,10)
     const askAndBidList = JSON.parse(JSON.stringify(res))
 
     setState({
-      Asks: askAndBidList.asks.filter((ask: Ask) => ask.amount !== 0),
-      Bids: askAndBidList.bids.filter((bid: Bid) => bid.amount !== 0)
+      Asks: askAndBidList.asks.reverse().filter((ask: Ask) => ask.amount !== 0),
+      Bids: askAndBidList.bids.reverse().filter((bid: Bid) => bid.amount !== 0)
     });
   }
 
