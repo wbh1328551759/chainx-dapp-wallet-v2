45a51d2a92b0a87cce0e00b9a8bd2654bf2a66d9
吴wbh1328551759 <1328551759@qq.com>
2020-12-17 14:16:47 +0800

    fix limit of of tx xGatewayBitcoin_signWithdrawTx(tx)

--- packages/react-components-chainx/src/index.tsx
+++ packages/react-components-chainx/src/index.tsx
@@ -1,3 +1,4 @@
 export { default as InputXBTCBalance } from './InputXBTCBalance'
 export { default as AccountName } from './AccountName'
 export { default as AddressSmall } from './AddressSmall'
+export { default as Extrinsic } from './Extrinsic'
