48b24036f1ad4f76586e2e3d69cadfb48d6aca20
吴wbh1328551759 <1328551759@qq.com>
2020-12-28 14:34:10 +0800

    add divideLine in subTab

--- packages/react-components-chainx/src/Tabs/SubTabs.tsx
+++ packages/react-components-chainx/src/Tabs/SubTabs.tsx
@@ -8,6 +8,13 @@ const Wrapper = styled.div`
   right: 1.3em;
   display: flex;
   top: 1.9em;
+
+  .divideLine{
+    height: 1.5em;
+    padding: 0;
+    background: #ADADAD;
+    width: 1px;
+  }
 `
 
 interface Props {
@@ -21,7 +28,10 @@ function SubTabs({subItems, basePath, name}: Props): React.ReactElement<Props>{
   return (
     <Wrapper>
       {subItems.map((item: subItem, index: number) =>
+        <>
         <SubTab {...item} basePath={basePath} name={name}/>
+        {subItems.length - 1 !== index ? <div className='divideLine'/>: null}
+        </>
       )}
     </Wrapper>
   )
