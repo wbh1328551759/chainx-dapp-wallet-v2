d8f3810020ac6f0f3427a860938205145696e826
吴wbh1328551759 <1328551759@qq.com>
2020-12-29 11:40:02 +0800

    add list of account in contract

--- packages/page-accounts-chainx/src/Myview/Records/Contacts/Account.tsx
+++ packages/page-accounts-chainx/src/Myview/Records/Contacts/Account.tsx
@@ -1,34 +1,132 @@
-import React from 'react';
+import React, {useCallback, useContext, useState} from 'react';
 import Row from '@polkadot/react-components/Row';
 import {toShortAddress} from '@polkadot/react-components/util';
 import {DEFAULT_ADDR, Props} from '@polkadot/react-components/AddressRow';
-import {useAccountInfo} from '@polkadot/react-hooks';
+import {useAccountInfo, useApi, useCall, useToggle} from '@polkadot/react-hooks';
 import IdentityIcon from '@polkadot/react-components/IdentityIcon';
 import BaseIdentityIcon from '@polkadot/react-identicon';
-import styled from 'styled-components';
-import { Icon } from '@polkadot/react-components'
+import {Button, ChainLock, Forget, Icon, Menu, Popup, StatusContext} from '@polkadot/react-components';
 import {AccountWrapper} from './Wrapper'
+import {createMenuGroup} from '@polkadot/app-accounts-chainx/util';
+import {ThemeDef} from '@polkadot/react-components/types';
+import {ThemeContext} from 'styled-components';
+import {useTranslation} from '@polkadot/app-accounts-chainx/translate';
+import {SubmittableExtrinsic} from '@polkadot/api/types';
+import BN from 'bn.js';
+import {getLedger} from '@polkadot/react-api';
+import {KeyringAddress} from '@polkadot/ui-keyring/types';
+import {AssetsInfo} from '@polkadot/react-hooks/types';
+import {Delegation} from '@polkadot/app-accounts-chainx/types';
+import {ProxyDefinition, RecoveryConfig} from '@polkadot/types/interfaces';
+import useMultisigApprovals from '@polkadot/app-accounts-chainx/Accounts/useMultisigApprovals';
+import {Option} from '@polkadot/types';
+import Backup from '@polkadot/app-accounts-chainx/modals/Backup';
+import DelegateModal from '@polkadot/app-accounts-chainx/modals/Delegate';
+import Derive from '@polkadot/app-accounts-chainx/modals/Derive';
+import Withdraw from '@polkadot/app-accounts-chainx/modals/withdraw';
+import XbtcTransfer from '@polkadot/app-accounts-chainx/modals/XBTCTransfer';
+import IdentityMain from '@polkadot/app-accounts-chainx/modals/IdentityMain';
+import IdentitySub from '@polkadot/app-accounts-chainx/modals/IdentitySub';
+import ChangePass from '@polkadot/app-accounts-chainx/modals/ChangePass';
+import Transfer from '@polkadot/app-accounts-chainx/modals/Transfer';
+import Deposite from '@polkadot/app-accounts-chainx/modals/deposite';
+import ProxyOverview from '@polkadot/app-accounts-chainx/modals/ProxyOverview';
+import MultisigApprove from '@polkadot/app-accounts-chainx/modals/MultisigApprove';
+import RecoverAccount from '@polkadot/app-accounts-chainx/modals/RecoverAccount';
+import RecoverSetup from '@polkadot/app-accounts-chainx/modals/RecoverSetup';
+import UndelegateModal from '@polkadot/app-accounts-chainx/modals/Undelegate';
 
 const ICON_SIZE = 36;
 
-function Account({buttons, children, className, defaultName, fullLength = false, isContract = false, isDisabled, isEditableName, isInline, isValid: propsIsValid, overlay, value, withTags = false}: Props): React.ReactElement<Props> | null {
-  const {accountIndex, isNull, name, onSaveName, onSaveTags, setName, setTags, tags} = useAccountInfo(value ? value.toString() : null, isContract);
-  const isValid = !isNull && (propsIsValid || value || accountIndex);
-  const InfoIcon = value ? IdentityIcon : BaseIdentityIcon;
-  const address = value && isValid ? value : DEFAULT_ADDR;
+interface DemocracyUnlockable {
+  democracyUnlockTx: SubmittableExtrinsic<'promise'> | null;
+  ids: BN[];
+}
 
+interface AccountProps extends Props{
+  account: KeyringAddress;
+  assetsInfo: AssetsInfo;
+  className?: string;
+  delegation?: Delegation;
+  proxy?: [ProxyDefinition[], BN];
 
+}
+
+const transformRecovery = {
+  transform: (opt: Option<RecoveryConfig>) => opt.unwrapOr(null)
+};
+
+function Account({account: {address, meta}, proxy, delegation, buttons, children, className, defaultName, fullLength = false, isContract = false, isDisabled, isEditableName, isInline, isValid: propsIsValid, overlay, withTags = false}: AccountProps): React.ReactElement<AccountProps> | null {
+  const {t} = useTranslation();
+  const api = useApi();
+  const {flags: {isDevelopment, isExternal, isHardware, isInjected, isMultisig, isProxied}, accountIndex, isNull, name, onSaveName, onSaveTags, setName, setTags, identity, tags} = useAccountInfo(address ? address.toString() : null, isContract);
+  const isValid = !isNull && (propsIsValid || address || accountIndex);
+  const InfoIcon = address ? IdentityIcon : BaseIdentityIcon;
+  const newAddress = address && isValid ? address : DEFAULT_ADDR;
+  const {theme} = useContext<ThemeDef>(ThemeContext);
+  const [isSettingsOpen, toggleSettings] = useToggle();
+  const [isDepositeOpen, toggleDeposite] = useToggle();
+  const [isWithdraw, toggleWithdraw] = useToggle();
+  const [isXbtcTransfer, toggleXbtcTransfer] = useToggle();
+  const [isIdentityMainOpen, toggleIdentityMain] = useToggle();
+  const [isIdentitySubOpen, toggleIdentitySub] = useToggle();
+  const [{democracyUnlockTx}, setUnlockableIds] = useState<DemocracyUnlockable>({democracyUnlockTx: null, ids: []});
+  const [vestingVestTx, setVestingTx] = useState<SubmittableExtrinsic<'promise'> | null>(null);
+  const {queueExtrinsic} = useContext(StatusContext);
+  const [isDeriveOpen, toggleDerive] = useToggle();
+  const [isPasswordOpen, togglePassword] = useToggle();
+  const [isBackupOpen, toggleBackup] = useToggle();
+  const [isForgetOpen, toggleForget] = useToggle();
+  const [isRecoverSetupOpen, toggleRecoverSetup] = useToggle();
+  const [isRecoverAccountOpen, toggleRecoverAccount] = useToggle();
+  const [isMultisigOpen, toggleMultisig] = useToggle();
+  const [isDelegateOpen, toggleDelegate] = useToggle();
+  const [isUndelegateOpen, toggleUndelegate] = useToggle();
+  const [isProxyOverviewOpen, toggleProxyOverview] = useToggle();
+  const [n, setN] = useState<number>(0)
+
+  const recoveryInfo = useCall<RecoveryConfig | null>(api.api.query.recovery?.recoverable, [address], transformRecovery);
+  const multiInfos = useMultisigApprovals(address);
+
+  const _clearDemocracyLocks = useCallback(
+    () => democracyUnlockTx && queueExtrinsic({
+      accountId: address,
+      extrinsic: democracyUnlockTx
+    }),
+    [address, democracyUnlockTx, queueExtrinsic]
+  );
+
+  const _vestingVest = useCallback(
+    () => vestingVestTx && queueExtrinsic({
+      accountId: address,
+      extrinsic: vestingVestTx
+    }),
+    [address, queueExtrinsic, vestingVestTx]
+  );
+
+  const _showOnHardware = useCallback(
+    // TODO: we should check the hardwareType from metadata here as well,
+    // for now we are always assuming hardwareType === 'ledger'
+    (): void => {
+      getLedger()
+        .getAddress(true, meta.accountOffset as number || 0, meta.addressOffset as number || 0)
+        .catch((error): void => {
+          console.error(`ledger: ${(error as Error).message}`);
+        });
+    },
+    [meta]
+  );
   return (
     <AccountWrapper>
       <Row
-        address={fullLength ? address : toShortAddress(address)}
+        address={fullLength ? newAddress : toShortAddress(newAddress)}
         buttons={buttons}
         className={className}
         defaultName={defaultName}
         icon={
           <InfoIcon
             size={ICON_SIZE}
-            value={value ? value.toString() : null}
+            value={address ? address.toString() : null}
           />
         }
         isDisabled={isDisabled}
@@ -45,8 +143,311 @@ function Account({buttons, children, className, defaultName, fullLength = false,
         {children}
         {overlay}
       </Row>
+      {isBackupOpen && (
+        <Backup
+          address={address}
+          key='modal-backup-account'
+          onClose={toggleBackup}
+        />
+      )}
+      {isDelegateOpen && (
+        <DelegateModal
+          key='modal-delegate'
+          onClose={toggleDelegate}
+          previousAmount={delegation?.amount}
+          previousConviction={delegation?.conviction}
+          previousDelegatedAccount={delegation?.accountDelegated}
+          previousDelegatingAccount={address}
+        />
+      )}
+      {isDeriveOpen && (
+        <Derive
+          from={address}
+          key='modal-derive-account'
+          onClose={toggleDerive}
+        />
+      )}
+      {/*{*/}
+      {/*  isWithdraw && (*/}
+      {/*    <Withdraw*/}
+      {/*      account={address}*/}
+      {/*      btc={assetsInfo.Usable}*/}
+      {/*      onClose={toggleWithdraw}*/}
+      {/*      setN={setN}*/}
+      {/*    />*/}
 
-      <Icon icon='ellipsis-h'/>
+      {/*  )*/}
+      {/*}*/}
+      {
+        isXbtcTransfer && (
+          <XbtcTransfer
+            onClose={toggleXbtcTransfer}
+          />
+        )
+
+      }
+      {/*{isForgetOpen && (*/}
+      {/*  <Forget*/}
+      {/*    address={address}*/}
+      {/*    key='modal-forget-account'*/}
+      {/*    onClose={toggleForget}*/}
+      {/*    onForget={_onForget}*/}
+      {/*  />*/}
+      {/*)}*/}
+      {isIdentityMainOpen && (
+        <IdentityMain
+          address={address}
+          key='modal-identity-main'
+          onClose={toggleIdentityMain}
+        />
+      )}
+      {isIdentitySubOpen && (
+        <IdentitySub
+          address={address}
+          key='modal-identity-sub'
+          onClose={toggleIdentitySub}
+        />
+      )}
+      {isPasswordOpen && (
+        <ChangePass
+          address={address}
+          key='modal-change-pass'
+          onClose={togglePassword}
+        />
+      )}
+
+      {/*{*/}
+      {/*  isDepositeOpen && (*/}
+      {/*    <*/}
+      {/*      Deposite*/}
+      {/*      address={address}*/}
+      {/*      onClose={toggleDeposite}*/}
+      {/*    />*/}
+      {/*  )*/}
+
+      {/*}*/}
+      {isProxyOverviewOpen && (
+        <ProxyOverview
+          key='modal-proxy-overview'
+          onClose={toggleProxyOverview}
+          previousProxy={proxy}
+          proxiedAccount={address}
+        />
+      )}
+      {isMultisigOpen && multiInfos && (
+        <MultisigApprove
+          address={address}
+          key='multisig-approve'
+          onClose={toggleMultisig}
+          ongoing={multiInfos}
+          threshold={meta.threshold as number}
+          who={meta.who as string[]}
+        />
+      )}
+      {isRecoverAccountOpen && (
+        <RecoverAccount
+          address={address}
+          key='recover-account'
+          onClose={toggleRecoverAccount}
+        />
+      )}
+      {isRecoverSetupOpen && (
+        <RecoverSetup
+          address={address}
+          key='recover-setup'
+          onClose={toggleRecoverSetup}
+        />
+      )}
+      {isUndelegateOpen && (
+        <UndelegateModal
+          accountDelegating={address}
+          key='modal-delegate'
+          onClose={toggleUndelegate}
+        />
+      )}
+      <Popup
+        className={`theme--${theme}`}
+        isOpen={isSettingsOpen}
+        onClose={toggleSettings}
+        trigger={
+          <Icon icon='ellipsis-h' onClick={toggleSettings}/>
+        }
+      >
+        <Menu
+          onClick={toggleSettings}
+          text
+          vertical
+        >
+          {createMenuGroup([
+
+            (
+              <Menu.Item
+                key='xbtc recharge'
+                onClick={toggleDeposite}
+              >
+                {t('XBTC recharge')}
+              </Menu.Item>
+
+            ),
+            (
+              <Menu.Item
+                key='xbtc withdraw'
+                onClick={toggleWithdraw}
+              >
+                {t('XBTC withdrawals')}
+              </Menu.Item>
+            ),
+            (
+              <Menu.Item
+                key='xbtc transfer'
+                onClick={toggleXbtcTransfer}
+              >
+                {t('XBTC Transfer')}
+              </Menu.Item>
+            ),
+            api.api.tx.identity?.setIdentity && (
+              <Menu.Item
+                key='identityMain'
+                onClick={toggleIdentityMain}
+              >
+                {t('Set on-chain identity')}
+              </Menu.Item>
+            ),
+            api.api.tx.identity?.setSubs && identity?.display && (
+              <Menu.Item
+                key='identitySub'
+                onClick={toggleIdentitySub}
+              >
+                {t('Set on-chain sub-identities')}
+              </Menu.Item>
+            ),
+            api.api.tx.democracy?.unlock && democracyUnlockTx && (
+              <Menu.Item
+                key='clearDemocracy'
+                onClick={_clearDemocracyLocks}
+              >
+                {t('Clear expired democracy locks')}
+              </Menu.Item>
+            ),
+            api.api.tx.vesting?.vest && vestingVestTx && (
+              <Menu.Item
+                key='vestingVest'
+                onClick={_vestingVest}
+              >
+                {t('Unlock vested amount')}
+              </Menu.Item>
+            )
+          ])}
+          {createMenuGroup([
+            !(isExternal || isHardware || isInjected || isMultisig) && (
+              <Menu.Item
+                key='deriveAccount'
+                onClick={toggleDerive}
+              >
+                {t('Derive account via derivation path')}
+              </Menu.Item>
+            ),
+            isHardware && (
+              <Menu.Item
+                key='showHwAddress'
+                onClick={_showOnHardware}
+              >
+                {t('Show address on hardware device')}
+              </Menu.Item>
+            )
+          ])}
+          {createMenuGroup([
+            !(isExternal || isInjected || isMultisig || isDevelopment) && (
+              <Menu.Item
+                key='backupJson'
+                onClick={toggleBackup}
+              >
+                {t('Create a backup file for this account')}
+              </Menu.Item>
+            ),
+            !(isExternal || isInjected || isMultisig || isDevelopment) && (
+              <Menu.Item
+                key='changePassword'
+                onClick={togglePassword}
+              >
+                {t('Change this account\'s password')}
+              </Menu.Item>
+            ),
+            !(isInjected || isDevelopment) && (
+              <Menu.Item
+                key='forgetAccount'
+                onClick={toggleForget}
+              >
+                {t('Forget this account')}
+              </Menu.Item>
+            )
+          ])}
+          {api.api.tx.recovery?.createRecovery && createMenuGroup([
+            !recoveryInfo && (
+              <Menu.Item
+                key='makeRecoverable'
+                onClick={toggleRecoverSetup}
+              >
+                {t('Make recoverable')}
+              </Menu.Item>
+            ),
+            <Menu.Item
+              key='initRecovery'
+              onClick={toggleRecoverAccount}
+            >
+              {t('Initiate recovery for another')}
+            </Menu.Item>
+          ])}
+          {api.api.tx.multisig?.asMulti && isMultisig && createMenuGroup([
+            <Menu.Item
+              disabled={!multiInfos || !multiInfos.length}
+              key='multisigApprovals'
+              onClick={toggleMultisig}
+            >
+              {t('Multisig approvals')}
+            </Menu.Item>
+          ])}
+          {api.api.query.democracy?.votingOf && delegation?.accountDelegated && createMenuGroup([
+            <Menu.Item
+              key='changeDelegate'
+              onClick={toggleDelegate}
+            >
+              {t('Change democracy delegation')}
+            </Menu.Item>,
+            <Menu.Item
+              key='undelegate'
+              onClick={toggleUndelegate}
+            >
+              {t('Undelegate')}
+            </Menu.Item>
+          ])}
+          {api.api.query.democracy?.votingOf && !delegation?.accountDelegated && createMenuGroup([
+            <Menu.Item
+              key='delegate'
+              onClick={toggleDelegate}
+            >
+              {t('Delegate democracy votes')}
+            </Menu.Item>
+          ])}
+          {api.api.query.proxy?.proxies && createMenuGroup([
+            <Menu.Item
+              key='proxy-overview'
+              onClick={toggleProxyOverview}
+            >
+              {proxy?.[0].length
+                ? t('Manage proxies')
+                : t('Add proxy')
+              }
+            </Menu.Item>
+          ])}
+          {/*<ChainLock*/}
+          {/*  className='accounts--network-toggle'*/}
+          {/*  genesisHash={genesisHash}*/}
+          {/*  isDisabled={api.isDevelopment}*/}
+          {/*  onChange={onSetGenesisHash}*/}
+          {/*/>*/}
+        </Menu>
+      </Popup>
     </AccountWrapper>
   );
 }
--- packages/page-accounts-chainx/src/Myview/Records/Contacts/index.tsx
+++ packages/page-accounts-chainx/src/Myview/Records/Contacts/index.tsx
@@ -1,20 +1,42 @@
-import React from 'react';
-import { useAccounts, useLoadingDelay} from '@polkadot/react-hooks';
+import React, {useContext} from 'react';
+import {useAccounts, useLoadingDelay, useToggle} from '@polkadot/react-hooks';
 import Account from './Account'
-import {Icon} from '@polkadot/react-components';
+import {Icon, StatusContext} from '@polkadot/react-components';
 import {Wrapper} from './Wrapper'
+import CreateModal from '@polkadot/app-accounts-chainx/modals/Create';
+import {sortAccounts} from '@polkadot/app-accounts-chainx/util';
+import {useAccountAssets} from '@polkadot/react-hooks-chainx';
 
 function Contacts(){
   const isLoading = useLoadingDelay();
   const { allAccounts, hasAccounts } = useAccounts();
+  const [isCreateOpen, toggleCreate] = useToggle();
+  const {queueAction} = useContext(StatusContext);
+  const sortedAccounts = sortAccounts(allAccounts, []);
+  const { allAssets } = useAccountAssets([]);
 
+  console.log('sortedAccounts:',sortedAccounts)
   return (
     <Wrapper>
-      <button className='addAccountBtn'>
+      {isCreateOpen && (
+        <CreateModal
+          onClose={toggleCreate}
+          onStatusChange={queueAction}
+        />
+      )}
+
+      <button className='addAccountBtn' onClick={toggleCreate}>
         <Icon icon='plus'/>
       </button>
-      {isLoading ? undefined : (hasAccounts && allAccounts?.map((account, index): React.ReactNode => (
-        <Account value={account} key={index}/>
+      {isLoading ? undefined : (hasAccounts && sortedAccounts?.map(({account}, index): React.ReactNode => (
+        <Account
+          account={account}
+          key={index}
+          assetsInfo={allAssets && allAssets.length > index ? allAssets.find(
+            (assets) => assets.account === account.address
+          ) : {}}
+          value={account.address}
+        />
       )))}
     </Wrapper>
   )
