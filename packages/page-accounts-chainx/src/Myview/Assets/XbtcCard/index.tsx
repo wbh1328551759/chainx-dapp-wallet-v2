0ed373e2cb0b9b5e8353eba9ed5997647304c151
吴wbh1328551759 <1328551759@qq.com>
2020-12-29 11:55:43 +0800

    package useXbtcAssets

--- packages/page-accounts-chainx/src/Myview/Assets/XbtcCard/index.tsx
+++ packages/page-accounts-chainx/src/Myview/Assets/XbtcCard/index.tsx
@@ -14,66 +14,19 @@ import {useTranslation} from '@polkadot/app-accounts/translate';
 import {AccountContext} from '@polkadot/react-components-chainx/AccountProvider';
 import Button from '@polkadot/react-components-chainx/Button';
 import {useLocalStorage} from '@polkadot/react-hooks-chainx';
+import useXbtcAssets from '@polkadot/app-accounts-chainx/Myview/useXbtcAssets';
 
 
 export default function (): React.ReactElement {
-  const {api, isApiReady} = useApi();
+  const {isApiReady} = useApi();
   const {t} = useTranslation();
   const {currentAccount} = useContext(AccountContext);
   const [isTransferOpen, toggleTransfer] = useToggle();
   const [isDepositeOpen, toggleDeposite] = useToggle();
   const [isWithdraw, toggleWithdraw] = useToggle();
-  const [currentAccountInfo, setCurrentAccountInfo] = useState<AssetsInfo>();
   const [n, setN] = useState(0);
   const {hasAccounts} = useAccounts()
-  const [, setValue] = useLocalStorage('xbtcInfo');
-
-  useEffect((): void => {
-    async function getAssets(account: string): Promise<any> {
-      const res = await api.rpc.xassets.getAssetsByAccount(account);
-      let current = {
-        Locked: '0',
-        Reserved: '0',
-        ReservedDexSpot: '0',
-        ReservedWithdrawal: '0',
-        Usable: '0'
-      } as AssetsInfo;
-      const userAssets = JSON.parse(res);
-      Object.keys(userAssets).forEach((key: string) => {
-        current = userAssets[key] as AssetsInfo;
-      });
-
-      const dividendRes = await api.rpc.xminingasset.getDividendByAccount(
-        account
-      );
-      let currentDividend: any = '0';
-      const userDividend = JSON.parse(dividendRes);
-
-      Object.keys(userDividend).forEach((key: string) => {
-        currentDividend = userDividend[key];
-      });
-
-      if (JSON.stringify(current) === '{}') {
-        current = {
-          Locked: '0',
-          Reserved: '0',
-          ReservedDexSpot: '0',
-          ReservedWithdrawal: '0',
-          Usable: '0'
-        } as AssetsInfo;
-      }
-
-      current = Object.assign(current, {
-        account: account,
-        assetName: 'X-BTC',
-        XbtcInterests: currentDividend
-      });
-      setValue(JSON.stringify(current));
-      setCurrentAccountInfo(current);
-    }
-
-    getAssets(currentAccount);
-  }, [currentAccount, n]);
+  const currentAccountInfo = useXbtcAssets(currentAccount, n)
 
   const buttonGroup = (
     <div style={{display: 'flex', alignItems: 'center', height: '100%'}}>
