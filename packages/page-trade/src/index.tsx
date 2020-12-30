33e12814669069975c5bd8e818b683acb23a8460
吴wbh1328551759 <1328551759@qq.com>
2020-12-30 13:58:47 +0800

    fix tab in dex

--- packages/page-trade/src/index.tsx
+++ packages/page-trade/src/index.tsx
@@ -2,20 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import {AppProps as Props} from '@polkadot/react-components/types';
-import React, {useRef, useState, useContext} from 'react';
-import {Tabs} from '@polkadot/react-components';
+import React, {useRef} from 'react';
+import {Tabs} from '@polkadot/react-components-chainx';
 import {Route, Switch} from 'react-router';
 import Wrapper from './Wrapper';
 import Trade from './Module';
 import Orders from './Orders';
 import {useTranslation} from './translate';
-import {AccountContext} from '@polkadot/react-components-chainx/AccountProvider';
 import {DexProvider} from '@polkadot/react-components-chainx/DexProvider';
 
 function TradeApp({basePath}: Props): React.ReactElement<Props> {
   const {t} = useTranslation();
-  const [, setNodeName] = useState<string>('');
-  const {currentAccount} = useContext(AccountContext);
 
   const itemsRef = useRef([
     {
