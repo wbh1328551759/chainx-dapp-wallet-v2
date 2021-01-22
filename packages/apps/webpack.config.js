1a34a0376ebe250488d3fad211a95f27803209bc
吴wbh1328551759 <1328551759@qq.com>
2021-01-22 10:48:19 +0800

    use dotenv in webpack.config and add ali-oss

--- packages/apps/webpack.config.js
+++ packages/apps/webpack.config.js
@@ -3,14 +3,18 @@
 
 const fs = require('fs');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const AliosscdnWebpackPlugin = require('aliosscdn-webpack-plugin')
 const path = require('path');
 const { merge } = require('webpack-merge');
-
 const baseConfig = require('./webpack.base.config');
+const dotenv = require("dotenv")
+dotenv.config()
 
 const ENV = process.env.NODE_ENV || 'development';
 const context = __dirname;
 const hasPublic = fs.existsSync(path.join(context, 'public'));
+
+const CLIENT = JSON.parse(process.env.CLIENT)
 
 module.exports = merge(
   baseConfig(ENV, context),
@@ -21,6 +25,13 @@ module.exports = merge(
         PAGE_TITLE: 'ChainX Dapp Wallet',
         inject: true,
         template: path.join(context, `${hasPublic ? 'public/' : ''}index.html`)
+      }),
+      new AliosscdnWebpackPlugin({
+        filesPath: `${__dirname}/build`,
+        region: CLIENT.region,
+        accessKeyId: CLIENT.accessKeyId,
+        accessKeySecret: CLIENT.accessKeySecret,
+        bucket: CLIENT.bucket,
       })
     ]
   }
